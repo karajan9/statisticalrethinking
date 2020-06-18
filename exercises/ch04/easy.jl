a86bd1f9f0906db8de27b9fc773e508d523ee65d
Karajan <jens.karajan@googlemail.com>
2020-06-18 07:55:21 +0200

    Messy start for the hard exercise

--- exercises/ch04/easy.jl
+++ exercises/ch04/easy.jl
@@ -2,15 +2,14 @@ using DrWatson
 @quickactivate "StatReth"
 
 # %%
-# using CSV
-# # using StatsBase
-# using Distributions
-# using DataFrames
-# using Turing
-# using Plots
-# using StatsPlots
-#
-# include(srcdir("quap.jl"))
+using CSV
+using Distributions
+using DataFrames
+using Turing
+using Plots
+using StatsPlots
+
+include(srcdir("quap.jl"))
 
 # %% 4E1
 """
@@ -30,17 +29,12 @@ P(A|B) = P(B|A) * P(A) / P(B)
 # %% 4E4
 
 # %% 4M1
-using Distributions
-
 N = 1000
 σ = rand(Exponential(1), N)
 μ = rand(Normal(0, 10), N)
 y = rand.(Normal.(μ, σ))
 
 # %% 4M2
-using Turing
-using DataFrames
-
 @model function M1()
     μ ~ Normal(0, 10)
     σ ~ Exponential(1)
@@ -50,9 +44,6 @@ end
 y2 = sample(M1(), Prior(), N) |> DataFrame
 
 # %%
-using Plots
-using StatsPlots
-
 density(y)
 density!(y2.y)
 
@@ -109,6 +100,125 @@ ages it pretty small.
 # %% 4M8
 
 # %% 4H1
+d = DataFrame(CSV.File(datadir("exp_raw/Howell_1.csv")))
+sort!(d, :weight)
+
+density(d.weight)
+scatter(d.weight, d.height)
+
+function zscore(data, s = data)
+    μ = mean(data)
+    σ = std(data)
+    z = (s .- μ) ./ σ
+    unz(d) = d .* σ .+ μ
+    return z, unz
+end
+
+d.weight_s, unz_weight = zscore(d.weight)
+# scatter(d.weight_s, d.height)
+
+
+using BSplines
+
+num_knots = 5
+knot_list = quantile(d.weight_s, range(0, 1, length = num_knots))
+
+Bspline = BSplineBasis(3, knot_list)
+B = basismatrix(Bspline, d.weight_s)
+
+# plot(legend = false, xlabel = "year", ylabel = "basis value")
+# for y in eachcol(B)
+#     plot!(d.weight_s, y)
+# end
+# plot!()
+
+
+@model function height(weight_s, height, B = B)
+    α ~ Normal(150, 20)
+    β ~ Normal(20, 20)
+    w ~ filldist(Normal(0, 10), size(B, 2))
+    σ ~ Exponential(1)
+    μ = α .+ β .* weight_s .+ B * w  # mean + linear + wiggles
+    height ~ MvNormal(μ, σ)
+end
+
+m = height(d.weight_s, d.height)
+
+prior = sample(m, Prior(), 1000) |> DataFrame
+w_str = "w[" .* string.(1:size(B, 2)) .* "]"
+mu = prior.α' .+ prior.β' .* d.weight_s .+ B * Array(prior[!, w_str])'
+
+mu_m = mean.(eachrow(mu))
+mu_lower = quantile.(eachrow(mu), 0.055)
+mu_upper = quantile.(eachrow(mu), 0.945)
+
+scatter(d.weight_s, d.height, alpha = 0.3, legend = false)
+plot!(d.weight_s, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
+
+
+
+q = quap(height(d.weight_s, d.height), NelderMead())
+post = DataFrame(rand(q.distr, 1000)', ["α"; w_str; "σ"])
+
+post = sample(height(d.weight_s, d.height), NUTS(), 1000)
+post = DataFrame(post)
+
+mu = post.α' .+ post.β' .* d.weight_s .+ B * Array(post[!, w_str])'
+
+mu_m = mean.(eachrow(mu))
+mu_lower = quantile.(eachrow(mu), 0.055)
+mu_upper = quantile.(eachrow(mu), 0.945)
+
+scatter(d.weight_s, d.height, alpha = 0.3, legend = false)
+plot!(d.weight_s, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
+
+
+
+
+# %% 4.70, 4.71
+weight_seq = range(-2.2, 2, length = 30)
+post = DataFrame(rand(m4_6.distr, 1_000)', ["a", "b1", "b2", "b3", "σ"])
+mu = f_cube.(weight_seq, post.a', post.b1', post.b2', post.b3')
+sim = rand.(Normal.(mu, post.σ'))
+
+mu_m = mean.(eachrow(mu))
+mu_lower = quantile.(eachrow(mu), 0.055)
+mu_upper = quantile.(eachrow(mu), 0.945)
+sim_m = mean.(eachrow(sim))
+sim_lower = quantile.(eachrow(sim), 0.055)
+sim_upper = quantile.(eachrow(sim), 0.945)
+
+weight_seq_rescaled = weight_seq .* std(d.weight) .+ mean(d.weight)
+scatter(d.weight, d.height, ms = 3, alpha = 0.7, legend = false)
+plot!(weight_seq_rescaled, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
+plot!(weight_seq_rescaled, sim_lower, fillrange = sim_upper, alpha = 0.3, linealpha = 0.0, c = 2)
+
+
+
+using BSplines
+
+Bspline = BSplineBasis(4, knot_list)
+B = basismatrix(Bspline, d2.year)
+
+plot(legend = false, xlabel = "year", ylabel = "basis value")
+for y in eachcol(B)
+    plot!(d2.year, y)
+end
+plot!()
+
+# %% 4.76
+@model function spline(D, B = B)
+    α ~ Normal(100, 10)
+    w ~ filldist(Normal(0, 10), size(B, 2))
+    σ ~ Exponential(1)
+    μ = α .+ B * w
+    D ~ MvNormal(μ, σ)
+    return μ
+end
+
+m4_6 = quap(spline(d2.doy))
+
+
 
 # %% 4H2
 
