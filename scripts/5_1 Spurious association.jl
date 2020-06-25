1c8b1b637ae92973bda2c0722cceae5c3d3512ff
Karajan <jens.karajan@googlemail.com>
2020-06-25 06:39:25 +0200

    Continue ch5.1

--- scripts/5_1 Spurious association.jl
+++ scripts/5_1 Spurious association.jl
@@ -64,8 +64,8 @@ lin_divorce_M(M, a, bM) = a .+ bM .* M
     D ~ MvNormal(μ, σ)
 end
 
-q5_1 = quap(divorce_M(d.M, d.D))
-post = DataFrame(rand(q5_1.distr, 1000)', ["a", "bM", "σ"])
+q5_2 = quap(divorce_M(d.M, d.D))
+post = DataFrame(rand(q5_2.distr, 1000)', ["a", "bM", "σ"])
 
 M_seq = range(-3, 3.2, length = 30)
 mu = lin_divorce_M(M_seq, post.a', post.bM')
@@ -78,30 +78,73 @@ plot!(M_seq, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
 missing  # TODO
 
 # %% 5.10
-lin_divorce_AM(A, M, a, bA, bM) = a .+ bA .* A .+ bM .* M
-@model function divorce_AM(A, M, D)
+@model function divorce_A_M(A, M, D)
     a ~ Normal(0, 0.2)
     bM ~ Normal(0, 0.5)
     bA ~ Normal(0, 0.5)
     σ ~ Exponential(1)
-    μ = lin_divorce_AM(A, M, a, bA, bM)
+    μ = a .+ bA .* A .+ bM .* M
     D ~ MvNormal(μ, σ)
 end
 
-m5_3 = divorce_AM(d.A, d.M, d.D)
+m5_3 = divorce_A_M(d.A, d.M, d.D)
 q5_3 = quap(m5_3)
 
+# %% 5.11
+# Well, this ain't pretty. But before I have something reasonable, I at least wanted to
+# something, even if it's messily thrown together. TODO
 
+x = [
+    q5_1.coef[2],       # bA
+    NaN,
+    q5_3.coef[3],
+    NaN,                # Spaceholder
+    NaN,                # bM
+    q5_2.coef[2],
+    q5_3.coef[2],
+]
+xerr = sqrt.([
+    q5_1.vcov[2, 2],    # bA
+    NaN,
+    q5_3.vcov[3, 3],
+    NaN,                # Spaceholder
+    NaN,                # bM
+    q5_2.vcov[2, 2],
+    q5_3.vcov[2, 2],
+])
+ylab = [
+    "bA m5.1",
+    "bA m5.2",
+    "bA m5.3",
+    "",
+    "bM m5.1",
+    "bM m5.2",
+    "bM m5.3",
+]
 
+scatter(x, ylab, xerr = xerr, legend = false)
+vline!([0])
 
-# %%
+# %% 5.12
+N = 50
+age = randn(N)
+mar = rand.(Normal.(-age))
+div = rand.(Normal.(age))
 
-q5_3 = quap(m5_3)
-post = DataFrame(rand(q5_3.distr, 1000)', ["a", "bM", "bA", "σ"])
+# %% 5.13
+lin_divorce_AM(A, a, bAM) = a .+ bAM .* A
+@model function divorce_AM(A, M)
+    a ~ Normal(0, 0.2)
+    bAM ~ Normal(0, 0.5)
+    σ ~ Exponential(1)
+    μ = lin_divorce_AM(A, a, bAM)
+    M ~ MvNormal(μ, σ)
+end
 
-M_seq = range(-3, 3.2, length = 30)
-mu = lin_divorce_M(M_seq, post.a', post.bM')
-mu_m, mu_lower, mu_upper = meanlowerupper(mu)
+q5_4 = quap(divorce_AM(d.A, d.M))
+post = DataFrame(rand(q5_4.distr, 1000)', ["a", "bAM", "σ"])
+mu_m, mu_lower, mu_upper = lin_divorce_AM(d.A, post.a', post.bAM') |> meanlowerupper
+resid = d.M .- mu_m
 
-scatter(d.M, d.D, alpha = 0.4, legend = false)
-plot!(M_seq, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
+scatter(d.A, d.M)
+plot!(d.A, mu_m, ribbon = (mu_m .- mu_lower, mu_upper .- mu_m))
